0d19139811d2c8f0668899a542d60f771530d10f
Meril Gandhi <meril.gandhi03@gmail.com>
2025-08-27 22:29:23 +0530

    added the logic of the color changer with JS file

---
+++ Infinite_color_changer/colorchanger.js
@@ -0,0 +1,22 @@
+const randomcolor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+let intervalId
+const startchangingcolor = function(){
+    if(!intervalId){intervalId = setInterval(changebg, 1000) }
+    function changebg(){
+    document.body.style.backgroundColor = randomcolor();
+    }
+}
+const stopchangingcolor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+document.querySelector("#start").addEventListener('click',startchangingcolor)
+document.querySelector("#stop").addEventListener('click',stopchangingcolor)
